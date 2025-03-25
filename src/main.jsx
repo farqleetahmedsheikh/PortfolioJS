ba3ee1de911279f918bdd08fb3545d2a860a3fab
Farqleet Ahmed Sheikh <farqleetahmedsheikh@users.noreply.github.com>
2025-03-25 15:15:40 +0500

    added vercel speed-insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 /** @format */
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <Analytics />
     <App />
+    <Analytics />
+    <SpeedInsights />
   </>
 );
